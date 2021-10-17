32301a0c03050c3874f7a3b1109e49cc6e1f419e
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2021-10-18 01:41:17 +0200

    Update script.js

--- front/API/script.js
+++ front/API/script.js
@@ -1,5 +1,23 @@
-const img = document.getElementById('img')
+fetch('http://localhost:3000/api/products')
+  .then(response => response.json())
+  .then(json => recupCanap(json));
 
-fetch("http://localhost:3000/images/kanap01.jpeg")
-    .then(res => res.json())
-    .then(data => img.src = data.url)
+function recupCanap(data){
+
+  let items = document.getElementById('items');
+
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+
+    let element = document.createElement('div');
+
+    element.innerHTML = `
+    <a href="#" class= product>
+    <img width= "270" height="270" src="${data[i]['imageUrl']}" alt="${data[i]['altTxt']}">
+    <h3 class="Name">${data[i]['name']}</h3>
+    <h2 class="Price">${data[i]['price']} €</h2>
+    </a>
+    `  
+    items.appendChild(element) 
+  }
+}
