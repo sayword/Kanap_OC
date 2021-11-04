aeea6f60afb894a092e32de10293365066a540d9
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2021-11-04 02:11:55 +0100

    Update product.js

--- front/API/product.js
+++ front/API/product.js
@@ -1,4 +1,7 @@
-fetch('http://localhost:3000/api/products/')
+let params = new URL(document.location).searchParams;
+let id = params.get("_id");
+
+fetch('http://localhost:3000/api/products/${id}')
     .then(response => response.json())
     .then(json => getPost(json));
 
@@ -7,22 +10,22 @@ fetch('http://localhost:3000/api/products/')
 
         let Img = document.querySelector(".item__img")
         let productImg = document.createElement("img");
-        productImg.innerHTML = '<img src="http://localhost:3000/images/kanap03.jpeg">'
+        productImg.src = article.imageUrl
         Img.appendChild(productImg)
 
         let Name = document.getElementById('title');
         let productName = document.createElement("h2");
-        productName.innerHTML = '<h2>Canapé</h2>'
+        productName.innerHTML = '<h2>$['name']</h2>'
         Name.appendChild(productName)
 
         let Price = document.getElementById('price');
         let productPrice = document.createElement("h3");
-        productPrice.innerHTML = '<h3>1300</h3>'
+        productPrice.innerHTML = '<h3>$['price']</h3>'
         Price.appendChild(productPrice)
 
         let Description = document.getElementById('description');
         let productDescription = document.createElement("p");
-        productDescription.innerHTML = '<p>Blablaa</p>'
+        productDescription.innerHTML = '<p>${data[i]['description']}</p>'
         Description.appendChild(productDescription)
 
 
