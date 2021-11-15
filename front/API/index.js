a2ca85621e238a047e61b1ac6b9f6e8deb6becae
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2021-11-15 10:10:15 +0100

    Update index.js

--- front/API/index.js
+++ front/API/index.js
@@ -1,26 +1,18 @@
-fetch('http://localhost:3000/api/products')
-  .then(response => response.json())
-  .then(json => extractName(json));
-
-
-function extractName(data){
-
-  let items = document.getElementById('items');
-
-
- 
-  for (let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-
-    let element = document.createElement('div');
-
-    element.innerHTML = `
-    <a href="./product.html?id= + ${data[i]["_id"]}" class= productLink>
-    <img width= "350" height="350" src="${data[i]['imageUrl']}" alt="${data[i]['altTxt']}">
-    <h3 class="productName">${data[i]['name']}</h3>
-    <h2 class="productPrice">${data[i]['price']} €</h2>
-    </a>
-    `  
-    items.appendChild(element)
-  }
-}
+fetch("http://localhost:3000/api/products/")
+.then(reponse => reponse.json())
+.then(data => {
+    for(product of data) {
+        console.log(product)
+        let div = document.createElement('div');
+        div.innerHTML =
+        `<a href="./product.html?id=${ product._id }">
+            <article>
+              <img src="${ product.imageUrl }" alt="${ product.atlTxt}">
+              <h4 class="name">${ product.name }</h4>
+              <p class="description">${ product.description }</p>
+              <h3 class="productPrice">${product.price} €</h3>
+            </article>
+          </a>`
+        items.appendChild(div)
+    }
+})
