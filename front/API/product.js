8d23d7655244a8e2758a8d44e9563b1ee97acd99
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2021-11-15 10:10:40 +0100

    Update product.js

--- front/API/product.js
+++ front/API/product.js
@@ -1,33 +1,20 @@
-let params = new URL(document.location).searchParams;
-let id = params.get("_id");
+const parametre = window.location.href;
+const kanapUrl = new URL(parametre);
+const id = kanapUrl.searchParams.get("id");
 
-fetch('http://localhost:3000/api/products/${id}')
-    .then(response => response.json())
-    .then(json => getPost(json));
-
-    function getPost(){
-
-
-        let Img = document.querySelector(".item__img")
-        let productImg = document.createElement("img");
-        productImg.src = article.imageUrl
-        Img.appendChild(productImg)
-
-        let Name = document.getElementById('title');
-        let productName = document.createElement("h2");
-        productName.innerHTML = '<h2>$['name']</h2>'
-        Name.appendChild(productName)
-
-        let Price = document.getElementById('price');
-        let productPrice = document.createElement("h3");
-        productPrice.innerHTML = '<h3>$['price']</h3>'
-        Price.appendChild(productPrice)
-
-        let Description = document.getElementById('description');
-        let productDescription = document.createElement("p");
-        productDescription.innerHTML = '<p>${data[i]['description']}</p>'
-        Description.appendChild(productDescription)
-
-
+fetch("http://localhost:3000/api/products/" + id)
+.then(function (res) {
+    if (res.ok) {
+      return res.json();
     }
-    
+  })
+  .then(data => {
+    document.querySelector("#title").innerHTML = data.name;
+    document.querySelector("#description").innerHTML = data.description;
+    document.querySelector("#price").innerHTML = data.price;
+    let img = document.querySelector(".item__img");
+    let image = document.createElement('img');
+    image.src = data.imageUrl;
+    img.appendChild(image);
+    });
+  
