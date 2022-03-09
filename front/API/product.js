ecc17c3facb1f3c402a213d4717684e49b05390c
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2022-03-09 13:40:59 +0100

    Update product.js

--- front/API/product.js
+++ front/API/product.js
@@ -1,11 +1,11 @@
 //Récupération de l'id dans l'url
 let param = window.location.href;
 let url = new URL(param);
-let productId = url.searchParams.get("id");
+let id = url.searchParams.get("id");
 
-console.log(productId)
+console.log(id)
 
-fetch("http://localhost:3000/api/products/" + productId)
+fetch("http://localhost:3000/api/products/" + id)
 	.then(function(res) {
 		if (res.ok) {
 			return res.json();
@@ -35,15 +35,53 @@ let registerItem = [];
 const addToCart = document.querySelector("#addToCart");
 // creation d'un evenement au click du btn
 addToCart.addEventListener("click", () => {
-	let productColor = document.querySelector("#colors").value;
-	let productQuantity = document.querySelector("#quantity").value;
+	let color = document.querySelector("#colors").value;
+	let qty = document.querySelector("#quantity").value;
 	// au click du btn on verifie les elements suivant : si l'utilisateur a bien choisit une couleur et une quantité
-	if (productColor == "") {
+	if (color == "") {
 		alert("Veuillez choisir une couleur");
-	} else if (productQuantity <= 0 || productQuantity >= 101) {
+	} else if (qty <= 0 || qty >= 101) {
 		alert("Choisissez la quantité compris entre 1 et 100 merci");
 	} else {
-
-		add2Cart(productId, productColor, parseInt(productQuantity));
+		alert("Votre element s'ajoute au panier")
+		//------------- FONCTION AJOUT PANIER-----------------------------
+		function add2Cart(id, color, qty) {
+			let productInCart = localStorage.getItem('productInCart');
+		  
+		  
+		  
+		  // si il n'y a rien dans le panier
+		  if (productInCart === null) {
+			let tabPanier = [
+			  [id, color, parseInt(qty), image]
+			];
+			let tabPanierStr = JSON.stringify(tabPanier)
+			localStorage.setItem('productInCart', tabPanierStr)
+		  }
+		  else {
+			//parse ici
+			productInCart = JSON.parse(productInCart);
+			let find = productInCart.find(
+			  (data) => data.id === id && data.color === color);
+			// sinon, si il a trouver le meme produit dans le panier, alors j'ajoute juste la quantité 
+			if (find) {
+			  let newQty = parseInt(qty) + parseInt(find.qty);
+			  find.qty = newQty;
+			  productInCart.push(find);
+			  let tabPanierStr = JSON.stringify(productInCart) // ici j'ai remplacer le tabpanier productInCart
+			  localStorage.setItem('productInCart', tabPanierStr)
+			} 
+			// ou sinon tu ajoute l'élément au panier en gardant les éléments qui sont déjà dans le panier
+			else {
+			  let tabPanier = [
+				[id, color, parseInt(qty)]
+			  ];
+			  productInCart.push(tabPanier);
+			  let tabPanierStr = JSON.stringify(tabPanier)
+			  localStorage.setItem('productInCart', tabPanierStr)
+			}
+		  }
+		  }
+		  add2Cart(id, color, qty)
 	}
 });
