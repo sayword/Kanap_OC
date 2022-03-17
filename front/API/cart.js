6d241a0ce6d3686aa5bf7cc308c6f33eeefc66bd
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2022-03-17 17:47:16 +0100

    Update cart.js

--- front/API/cart.js
+++ front/API/cart.js
@@ -1,56 +1,97 @@
 //------------fonction qui récupere les données du LocalStorage--------------
-
 //recup avec un fetch les données du produit
 
 //------------- FONCTION AJOUT PANIER-----------------------------
+function add2Cart(id, color, qty) {
+	let productInCart = localStorage.getItem('productInCart');
 
+
+
+	// si il n'y a rien dans le panier
+	if (productInCart === null) {
+
+		let tabPanier = {
+			'id': id,
+			'couleur': color,
+			'quantité': qty
+		};
+		let tabPanierStr = JSON.stringify(tabPanier)
+		localStorage.setItem('productInCart', tabPanierStr)
+	} else {
+		//parse ici
+
+		productInCart = JSON.parse(productInCart);
+		let find = productInCart.find(
+			(data) => data.id === id && data.color === color);
+		// sinon, si il a trouver le meme produit dans le panier, alors j'ajoute juste la quantité 
+		if (find) {
+
+			let newQty = parseInt(qty) + parseInt(find.qty);
+			find.qty = newQty;
+			productInCart.push(find);
+			let tabPanierStr = JSON.stringify(productInCart) // ici j'ai remplacer le tabpanier productInCart
+			localStorage.setItem('productInCart', tabPanierStr)
+		}
+		// ou sinon tu ajoute l'élément au panier en gardant les éléments qui sont déjà dans le panier
+		else {
+
+			let tabPanier = {
+				'id': id,
+				'couleur': color,
+				'quantité': qty
+			};
+			productInCart.push(tabPanier);
+			let tabPanierStr = JSON.stringify(productInCart)
+			localStorage.setItem('productInCart', tabPanierStr)
+		}
+	}
+}
 //-------fonction qui calcul la quantité total-----
 
 function totalQuantity() {
-  let totalQuantity = document.querySelector("#totalQuantity");
-  let quantityProduct = document.querySelector('.itemQuantity');
-  let totalQuantities = 0;
-  quantityProduct.forEach(quantity =>{
-  totalQuantities += Number(quantity.value);
-  })
+	let totalQuantity = document.querySelector("#totalQuantity");
+	let quantityProduct = document.querySelector('.itemQuantity');
+	let totalQuantities = 0;
+	quantityProduct.forEach(quantity => {
+		totalQuantities += Number(quantity.value);
+	})
 }
 
 //----------fonction qui calcul le prix total---------
 
 function totalPrice() {
-  let totalPrice = document.querySelector("#totalPrice");
-  let priceItem = document.querySelector(".priceItem")
-  total = 0;
-  priceItem.forEach(price => {
-      total += Number(price.textContent);
-  })
+	let totalPrice = document.querySelector("#totalPrice");
+	let priceItem = document.querySelector(".priceItem")
+	total = 0;
+	priceItem.forEach(price => {
+		total += Number(price.textContent);
+	})
 }
 
 // faire un DOM pour placer dans html
 // j'essaye de faire avec la méthode forEach
 productInCart.forEach(item => {
-        
-  let cartArticles = document.getElementById("cart__items")
 
-  let article = document.createElement("article");
-      article.classList.add("cart__item");
-      article.setAttribute("data-id", item.id)
-      cartArticles.appendChild(article);
-  
-  let cartImg = document.createElement("div");
-      cartImg.classList.add("cart__item__img");
-      article.appendChild(cartImg);
+	let cartArticles = document.getElementById("cart__items")
 
-  let imgItem = document.createElement("img");
-      cartImg.appendChild(imgItem);
-      imgItem.src = item.image;      
-      
-  // calcul qty total + prix total
-  totalPrice()
-  totalQuantity()
+	let article = document.createElement("article");
+	article.classList.add("cart__item");
+	article.setAttribute("data-id", item.id)
+	cartArticles.appendChild(article);
+
+	let cartImg = document.createElement("div");
+	cartImg.classList.add("cart__item__img");
+	article.appendChild(cartImg);
+
+	let imgItem = document.createElement("img");
+	cartImg.appendChild(imgItem);
+	imgItem.src = item.image;
+
+	// calcul qty total + prix total
+	totalPrice()
+	totalQuantity()
 
 });
-
 
 // -------------FORMULAIRE--------------
 // Recuperation des éléments + regex  
@@ -67,83 +108,99 @@ let regexAddress = /^[a-zA-Z0-9\s,'-]*$/;
 let errorAddress = document.getElementById('addressErrorMsg');
 
 let city = document.getElementById('city');
-let regexCity=/^[a-zA-Z\u0080-\u024F]+(?:([\ \-\']|(\.\ ))[a-zA-Z\u0080-\u024F]+)*$/;
+let regexCity = /^[a-zA-Z\u0080-\u024F]+(?:([\ \-\']|(\.\ ))[a-zA-Z\u0080-\u024F]+)*$/;
 let errorCity = document.getElementById('cityErrorMsg');
 
 let email = document.getElementById('email');
-let regexEmail=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 let errorEmail = document.getElementById('emailErrorMsg');
 
 
 //--------------------------------
 //Erreur en cas de non respect du regex
 
-firstName.addEventListener('input',(e)=>{
-  e.preventDefault();
-  if (regexName.test(firstName.value)==false) {
-      errorFirstName.innerHTML = "Veuillez saisir votre prénom";
-  }else{
-      errorFirstName.innerHTML = "";
-  }
+firstName.addEventListener('input', (e) => {
+	e.preventDefault();
+	if (regexName.test(firstName.value) == false) {
+		errorFirstName.innerHTML = "Veuillez saisir votre prénom";
+	} else {
+		errorFirstName.innerHTML = "";
+	}
 });
 
-lastName.addEventListener('input',(e)=>{
-  e.preventDefault();
-  if (regexName.test(lastName.value)==false) {
-      errorLastName.innerHTML = "Veuillez saisir votre nom";
-  }else{
-      errorLastName.innerHTML = "";
-  }
+lastName.addEventListener('input', (e) => {
+	e.preventDefault();
+	if (regexName.test(lastName.value) == false) {
+		errorLastName.innerHTML = "Veuillez saisir votre nom";
+	} else {
+		errorLastName.innerHTML = "";
+	}
 });
 
-address.addEventListener('input',(e)=>{
-  e.preventDefault();
-  if (regexAddress.test(address.value)==false) {
-      errorAddress.innerHTML = "Veuillez saisir une vraie adresse";
-  }else{
-      errorAddress.innerHTML = "";
-  }
+address.addEventListener('input', (e) => {
+	e.preventDefault();
+	if (regexAddress.test(address.value) == false) {
+		errorAddress.innerHTML = "Veuillez saisir une vraie adresse";
+	} else {
+		errorAddress.innerHTML = "";
+	}
 });
 
-city.addEventListener('input',(e)=>{
-  e.preventDefault();
-  if (regexCity.test(city.value)==false) {
-      errorCity.innerHTML = "Veuillez saisir une vraie ville";
-  }else{
-      errorCity.innerHTML = "";
-  }
+city.addEventListener('input', (e) => {
+	e.preventDefault();
+	if (regexCity.test(city.value) == false) {
+		errorCity.innerHTML = "Veuillez saisir une vraie ville";
+	} else {
+		errorCity.innerHTML = "";
+	}
 });
 
-email.addEventListener('input',(e)=>{
-  e.preventDefault();
-  if (regexEmail.test(email.value)==false) {
-      errorEmail.innerHTML = "Email incorrect";
-  }else{
-      errorEmail.innerHTML = "";
-  }
+email.addEventListener('input', (e) => {
+	e.preventDefault();
+	if (regexEmail.test(email.value) == false) {
+		errorEmail.innerHTML = "Email incorrect";
+	} else {
+		errorEmail.innerHTML = "";
+	}
 });
 
-//je recup l'id du bouton pour faire un event
 let order = document.getElementById('order');
-order.addEventListener('click',(event)=>{
-  event.preventDefault();
-  let contact = {
-      firstName : firstName.value,
-      lastName : lastName.value,
-      address : address.value,
-      city : city.value,
-      email : email.value,
-  }
+//je balaye le productInCart
+let panier = {};
+productInCart.forEach(e => {
+	panier.push(e.id)
+});
+//je recup l'id du bouton pour faire un event
+order.addEventListener('click', (event) => {
+	event.preventDefault();
+	let contact = {
+		firstName: firstName.value,
+		lastName: lastName.value,
+		address: address.value,
+		city: city.value,
+		email: email.value,
+	}
+  //ici je mélange les deux tableaux
+	let data = {
+		panier,
+		contact
+	};
 
-
-  // si le client n'a pas bien rempli les champs alors on affiche un message d'erreur
-  if (firstName.value === ""|| lastName.value === ""|| address.value === "" || city.value === "" || email.value === "") {
-      alert("Vous n'avez pas bien rempli le formulaire")
-        // sinon, j'envoi mon tableau     
-  }else{
-      fetch(('http://localhost:3000/api/products/order'),{
-          method: "POST",
-          headers :{'Accept':'application/json','Content-type':'application/json'},
-          body : JSON.stringify(contact)
-      })}
+	// si le client n'a pas bien rempli les champs alors on affiche un message d'erreur
+	if (firstName.value === "" || lastName.value === "" || address.value === "" || city.value === "" || email.value === "") {
+		alert("Vous n'avez pas bien rempli le formulaire")
+		// sinon, j'envoi mon tableau     
+	} else {
+		fetch(('http://localhost:3000/api/products/order'), {
+				method: "POST",
+				headers: {
+					'Accept': 'application/json',
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			})
+			.then(res => {
+				return res.json();
+			})
+	}
 });
