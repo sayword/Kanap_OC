fdad158e03a0f46087d36251f6666b3e3e23303d
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2022-04-29 20:42:50 +0200

    Add files via upload

---
+++ front/API/confirmation.js
@@ -0,0 +1,5 @@
+// Récupération orderId
+const params = new URL(document.location).searchParams;
+const orderId = params.get("orderId");
+// Affichage du numéro de commande
+document.getElementById("orderId").textContent = orderId;
